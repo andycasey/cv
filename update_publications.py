2d5500c5160f9646f94796d0f8416004d52b3065
Andy Casey <andycasey@gmail.com>
2017-01-02 17:26:34 +0000

    Fix publication overflow and number alignment (closes #1 #2)

--- update_publications.py
+++ update_publications.py
@@ -19,7 +19,10 @@ orcid, last_name = ("0000-0003-0174-0564", "Casey")
 
 # Text formatting.
 tex_path, tex_template_path = ("cv/publications.tex", "cv/publications.tex.template")
-tex_row_template = "\cvhonor{{{title}}}{{{authors}}}{{{year}}}{{{N}}}\n"
+tex_row_template = "\cvpublication{{ \href{{{url}}}{{{title}}} }}{{{authors}}}{{{year}}}{{{N}}}\n"
+
+def ads_url(article):
+    return "https://adsabs.harvard.edu/abs/{}".format(article.identifier[0])
 
 def format_authors(article):
     string = "; ".join(article.author[:10])
@@ -67,13 +70,13 @@ blanks = {
     "N_second_author_publications": len(groups["2nd"]),
     "N_nth_author_publications": len(groups["Nth"]),
     "first_author_publications_tex": u"\n".join(
-        [unicode(tex_row_template).format(title=a.title[0], N=n, authors=format_authors(a), year=a.year) \
+        [unicode(tex_row_template).format(title=a.title[0], N=n, authors=format_authors(a), year=a.year, url=ads_url(a)) \
             for n, a in enumerate(groups["1st"], start=1)]),
     "second_author_publications_tex": u"\n".join(
-        [unicode(tex_row_template).format(title=a.title[0], N=n, authors=format_authors(a), year=a.year) \
+        [unicode(tex_row_template).format(title=a.title[0], N=n, authors=format_authors(a), year=a.year, url=ads_url(a)) \
             for n, a in enumerate(groups["2nd"], start=1)]),
     "nth_author_publications_tex": u"\n".join(
-        [unicode(tex_row_template).format(title=a.title[0], N=n, authors=format_authors(a), year=a.year) \
+        [unicode(tex_row_template).format(title=a.title[0], N=n, authors=format_authors(a), year=a.year, url=ads_url(a)) \
             for n, a in enumerate(groups["Nth"], start=1)]),
 }
 
